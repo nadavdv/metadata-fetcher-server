35ff1eaaffbd30b9f368785a8844ecd228edc38c
nadavdv <nadavdv@gmail.com>
2024-08-19 18:07:24 +0300

    port fixing and cleaning

--- server.js
+++ server.js
@@ -9,22 +9,22 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors());
-app.use(express.json()); // for parsing application/json
+app.use(express.json());
 app.use(helmet());
 
-// Rate limit middleware
+// rate limit middleware
 const limiter = rateLimit({
     windowMs: 1000, // 1 second
-    max: 5, // Limit each IP to 5 requests per second
+    max: 5, // limit each IP to 5 requests per second
     message: "Too many requests, please try again later.",
   });
 
 // Apply the rate limiter to the /fetch-metadata route
 app.use('/fetch-metadata', limiter);
 
-// Route to fetch metadata
+// routing
 app.post('/fetch-metadata', async (req, res) => {
-    const { urls } = req.body; // Extract URLs from the request body
+    const { urls } = req.body; // extract URLs
 
     if (!Array.isArray(urls) || urls.length === 0) {
         return res.status(400).json({ error: "Invalid input. Please provide an array of URLs." });
@@ -34,11 +34,11 @@ app.post('/fetch-metadata', async (req, res) => {
         const metadataResults = await Promise.all(
             urls.map(async (url) => {
                 try {
-                    // Fetch the HTML of the URL
+                    // fetch html
                     const { data } = await axios.get(url);
                     const $ = cheerio.load(data);
 
-                    // Extract metadata
+                    // extract metadata
                     const title = $('head > title').text() || null;
                     const description = $('meta[name="description"]').attr('content') || null;
                     const image = $('meta[property="og:image"]').attr('content') || null;
@@ -56,4 +56,4 @@ app.post('/fetch-metadata', async (req, res) => {
     }
 });
 
-app.listen(5000, ()=> {console.log(`listening on port ${port}`)})
+app.listen(port, ()=> {console.log(`listening on port ${port}`)})
